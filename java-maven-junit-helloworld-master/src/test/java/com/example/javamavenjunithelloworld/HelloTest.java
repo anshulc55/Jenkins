0456e2172ba128e4b38ea68ebfbc1ce04a2812fd
Anshul Chauhan <anshulc55@gmail.com>
2019-01-22 21:21:38 +0530

    Resolved the build failure

--- java-maven-junit-helloworld-master/src/test/java/com/example/javamavenjunithelloworld/HelloTest.java
+++ java-maven-junit-helloworld-master/src/test/java/com/example/javamavenjunithelloworld/HelloTest.java
@@ -23,7 +23,7 @@ public class HelloTest {
 
         Hello hi = new Hello();
         hi.sayHello(stream);
-        assertThat(os.toString(), is(equalTo(Hello.HELLO + "\r\n")));
+        assertThat(os.toString(), is(equalTo(Hello.HELLO + "\n")));
     }
 
     @Test
@@ -36,7 +36,7 @@ public class HelloTest {
         hi.sayHello(stream);
 
         // Does it say "Hello!" three times?
-        String goal = Hello.HELLO + "\r\n" + Hello.HELLO + "\r\n" + Hello.HELLO + "\r\n";
+        String goal = Hello.HELLO + "\n" + Hello.HELLO + "\n" + Hello.HELLO + "\n";
         assertThat(os.toString(), is(equalTo(goal)));
     }
 
